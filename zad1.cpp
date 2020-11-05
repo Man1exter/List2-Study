6945140e80de5e281444d158322f526b32be729b
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-05 10:14:43 +0100

    complete first exercises on met y = 3 * x

--- zad1.cpp
+++ zad1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 
-int x,y;
+int x,y,z;
 
 
 for(x = 0; x < 10; x++){
@@ -15,18 +15,6 @@ y = 3 * x;
 cout << "for wyniki do 10 to: "<< y << endl;
 
 }
-
-do
-{
-    
-x = 0;
-y = 3 * x;
-x++;
-
-cout<< "do ... while wyniki do 10 to: "<< y <<endl;
-
-}
-while (x > 10);
 
 
     return 0;
