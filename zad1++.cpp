5a3242d29cf9c682e0e705482cdb9136242c845d
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-05 10:42:07 +0100

    complete three exefrcises with while metod to 10

---
+++ zad1++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int x,y;
+
+while(x < 10)
+{
+y = 3 * x;
+x++;
+
+cout<<"wyniki while  / do 10 to: "<<y<<endl; 
+
+
+}
+
+
+    return 0;
+}
