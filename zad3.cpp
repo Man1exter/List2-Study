9bf4ed0cfe5dcfd87de984c7aca1f10301a60e5a
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-06 09:26:38 +0100

    add eps evidebnce score to complete the task

--- zad3.cpp
+++ zad3.cpp
@@ -6,22 +6,46 @@ using namespace std;
 
 int main()
 {
-    int x; // argument pierwiastka
-    int p; // kolejne przybliżenie 
-    int p - 1; // poprzednie przybliżenie wyniku
-    int n; // wartość p dokladniejsza n
+    int x; // argument pierwiastka..
+    int p; // kolejne przybliżenie..
+    int pn; //kolejne przyblizenie..
+    int n; // wartość p dokladniejsza n..
+    int EPS; // wynik koncowy..
     
     cout << "Podaj wartość pierwiastka kwadratowego z metody iteracyjnej Newtona" << endl;
     cin >> x;
 
-     cout << "Podaj wartość n" << endl;
-    cin >> n;
+     cout << "Podaj wartość n: " << endl;
+     cin >> n;
 
-    p * n = p * p * n-1 + x / 2 * p * n - 1;
+    pn = (p * p * n - 1  + x) / (2 * p * n - 1);
 
-    cout << "wynik pn" << p << endl;
+     cout << "wynik p: " << p << endl;
+     cout << "wynik n: " << n << endl;
 
 
+     if(x > 1.0){
+
+       p = x/2.0;
+
+     } else {
+
+         p = x * 2.0;
+     }
+
+     cout << "wynik koncowy z if else do sprawdzenia: " << p << endl;
+
+     while(fabs(p = 0 - p) >= EPS)
+     {
+         
+       i++;
+       
+       p = (x + p*p) / (2 * p);
+
+
+     }
+   
+   cout << " wynik EPS: " << EPS << endl;
 
 
 
