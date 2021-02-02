ba8206b4466bf6d99ad17e598f5399e843e4743c
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-02 09:33:13 +0100

    connect drawio file with block structure

--- zad2+.cpp
+++ zad2+.cpp
@@ -10,23 +10,17 @@ int y = 0;
 
 do
 {
-
  cout<< x <<" * "<< y <<"  =  "<< x * y <<endl;
 x++;
-
   do 
   {
-
     cout<< x <<" * "<< y <<"  =  "<< x * y <<endl;
     y++;
-
   }
     while(y < 11);
             y=0;
 }
 while(x < 10);
 x=0;
-
-
     return 0;
 }
